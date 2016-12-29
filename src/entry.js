1716afaae95843b440ee69dbf741b716a8adb11c
softboy <zzu_softboy@163.com>
2016-12-29 08:18:58 +0800

    完成babel配置

--- src/entry.js
+++
@@ -1,6 +0,0 @@
-console.log("xiuxiux")
-var name = "xiuxiux";
-console.log(name);
-for(var i = 0; i < 10; i++){
-   console.log(i);
-}
---
+++ src/kernel/class/Base.js
@@ -0,0 +1,6 @@
+var name = "asdasdasd";
+function xiuxiu()
+{
+   console.log("xiuxiuxiux");
+   ;
+}
