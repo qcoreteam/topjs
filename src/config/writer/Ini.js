4e4ee78ed1599fa42e6bd55228eba2b3e58b1d96
softboy <zzu_softboy@163.com>
2017-03-11 18:06:34 +0800

    add config package files

---
+++ src/config/writer/Ini.js
@@ -0,0 +1,8 @@
+"use strict";
+/*
+ * TopJs Framework (http://www.topjs.org/)
+ *
+ * @link      http://github.com/qcoreteam/topjs for the canonical source repository
+ * @copyright Copyright (c) 2016-2017 QCoreTeam (http://www.qcoreteam.org)
+ * @license   http://www.topjs.org/license/new-bsd New BSD License
+ */
