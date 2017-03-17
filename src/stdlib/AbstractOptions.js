23cf2b85bad48a1b560acdaa304bda59737927a8
softboy <zzu_softboy@163.com>
2017-03-17 15:17:35 +0800

    finished TopJs.stdlib.ParametersInterface interface

--- src/stdlib/AbstractOptions.js
+++ src/stdlib/AbstractOptions.js
@@ -69,6 +69,9 @@ let abstractOptionsHandler = {
     }
 };
 
+/**
+ * @class TopJs.stdlib.AbstractOptions
+ */
 class AbstractOptions
 {
     /**
--- src/stdlib/ParametersInterface.js
+++ src/stdlib/ParametersInterface.js
@@ -6,3 +6,87 @@
  * @copyright Copyright (c) 2016-2017 QCoreTeam (http://www.qcoreteam.org)
  * @license   http://www.topjs.org/license/new-bsd New BSD License
  */
+
+TopJs.namespace("TopJs.stdlib");
+
+class ParametersInterface {
+    /**
+     * Constructor
+     *
+     * @param {Object} values
+     */
+    constructor(values = null)
+    {
+    }
+
+    /**
+     * From
+     *
+     * Allow deserialization from standard
+     *
+     * @param values
+     * @return mixed
+     */
+    fromArray(values)
+    {
+    }
+
+    /**
+     * From string
+     *
+     * Allow deserialization from raw body{} e.g., for PUT requests
+     *
+     * @param {String} string
+     * @return mixed
+     */
+    fromString(string)
+    {
+    }
+
+    /**
+     * To
+     *
+     * Allow serialization back to standard
+     *
+     * @return mixed
+     */
+    toArray()
+    {
+    }
+
+    /**
+     * To string
+     *
+     * Allow serialization to query format{} e.g., for PUT or POST requests
+     *
+     * @return mixed
+     */
+    toString()
+    {
+    }
+
+    /**
+     * Get
+     *
+     * @param {String} name
+     * @param {mixed} defaultValue
+     * @return {mixed}
+     */
+    get(name, defaultValue = null)
+    {
+    }
+
+    /**
+     * Set
+     *
+     * @param {String} name
+     * @param {mixed} value
+     * @return {TopJs.stdlib.ParametersInterface}
+     */
+    set(name, value)
+    {
+    }
+}
+
+TopJs.registerClass("TopJs.stdlib.ParametersInterface", ParametersInterface);
+module.exports = ParametersInterface;
